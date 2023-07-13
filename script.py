1a36d3f3fbfc8a0cde19edce05eef2de991114ed
Haddad Sarah <haddads686@gmail.com>
2023-07-13 15:52:23 +0100

    update

--- script.py
+++ script.py
@@ -25,18 +25,30 @@ speed(0)
 ht()
 colors = ["red","tomato","salmon", "orange","gold", "yellow","lime","wheat","olive","green","teal","cyan","coral","aquamarine","skyblue", "blue","indigo","violet", "purple", "white","plum", "pink","orchid"]
 
-countdown = 20
-i = countdown
-
-while i >= 0:
-    t.color(colors[i % len(colors)])
-    t.clear()
-    t.circle(200, (countdown - i) * 360 / countdown)
-    k.write(i, align='center', font=('Arial', 56, 'bold'))
-    update()
-    time.sleep(1)
-    k.clear()  # Clear the text
-    i -= 1
-
-play_sound("breath.mp3")
+# Paramètres de la minuterie Pomodoro pour l'entraînement (en secondes)
+total_time = 600  # Temps total d'entraînement en secondes (10 minutes)
+workout_time = 30  # Durée de l'entraînement en secondes
+rest_time = 10  # Durée du repos en secondes
+cycles = total_time // (workout_time + rest_time)
+for _ in range(cycles):
+    i = workout_time
+    while i >= 0:
+        t.color(colors[i % len(colors)])
+        t.clear()
+        t.circle(200, (workout_time - i) * 360 / workout_time)
+        k.write(i, align='center', font=('Arial', 56, 'bold'))
+        update()
+        time.sleep(1)
+        k.clear()  # Clear the text
+        i -= 1
+    i = rest_time
+    while i >= 0:
+        t.color("grey")
+        t.clear()
+        t.circle(200, (rest_time - i) * 360 / rest_time)
+        k.write(i, align='center', font=('Arial', 56, 'bold'))
+        update()
+        time.sleep(1)
+        k.clear()
+        i -= 1
 done()
